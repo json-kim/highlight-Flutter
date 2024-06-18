520b242bd65b34b4964cc0cae706b574edf86017
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-06-18 17:04:48 +0900

    Feat: TypeSafe 라우트 적용

--- lib/app/app_router.dart
+++ lib/app/app_router.dart
@@ -1,9 +1,16 @@
+import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:highlight_flutter/screen/main/main_screen.dart';
 
-final appRouter = GoRouter(routes: [
-  GoRoute(
-    path: '/',
-    builder: (context, state) => const MainScreen(),
-  ),
-]);
+part 'app_router.g.dart';
+
+final appRouter = GoRouter(routes: $appRoutes);
+
+@TypedGoRoute<MainScreenRoute>(path: '/')
+@immutable
+class MainScreenRoute extends GoRouteData {
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const MainScreen();
+  }
+}
