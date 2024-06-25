06011127ca8693c07a25ca57380b6efff82501ba
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-06-25 16:47:16 +0900

    Feat: 사진 모아보기 스크린 추가

---
+++ lib/screen/photo_grid/photo_grid_view.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:highlight_flutter/core/image/file_image_data.dart';
+import 'package:highlight_flutter/core/image/image_data.dart';
+import 'package:highlight_flutter/screen/common/error_image.dart';
+
+class PhotoGridView extends StatelessWidget {
+  const PhotoGridView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.builder(
+      gridDelegate:
+          const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 3),
+      itemBuilder: (context, index) {
+        return PhotoGridItem(
+          imageData: FileImageData(filePath: 'filePath'),
+        );
+      },
+      itemCount: 10,
+    );
+  }
+}
+
+class PhotoGridItem extends StatelessWidget {
+  const PhotoGridItem({required this.imageData, super.key});
+
+  final ImageData imageData;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+        child: InkWell(
+      onTap: () {
+        // TODO: 상세 화면 이동
+      },
+      child: imageData.toImageWidget(),
+    ));
+  }
+}
