0f84a91293eac974794b5453a5c0d19bc009f4d3
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-08-11 14:57:46 +0900

    Feat: 내용 텍스트 필드바 ui, 현재 내용 상태 프로바이더 추가

---
+++ lib/screen/highlight_edit/content_field_bar.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:highlight_flutter/screen/highlight_edit/state/current_content_provider.dart';
+
+class ContentFieldBar extends ConsumerWidget {
+  const ContentFieldBar({super.key});
+
+  static const maxContentLength = 1000;
+
+  static const contentTextStyle = TextStyle(fontSize: 14);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.all(8),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text('내용'),
+          const SizedBox(width: 8),
+          Expanded(
+              child: SizedBox(
+            height: 300,
+            child: TextField(
+              style: contentTextStyle,
+              maxLines: null,
+              expands: true,
+              cursorWidth: 1,
+              maxLength: maxContentLength,
+              onChanged: (value) =>
+                  ref.watch(currentContentProvider.notifier).state = value,
+            ),
+          ))
+        ],
+      ),
+    );
+  }
+}
