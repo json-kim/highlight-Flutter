c2271ac26151969a08d8da5c022ad553f9e9f0de
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-08-11 14:45:03 +0900

    Feat: 텍스트필드 데코레이션 머티리얼앱 테마로 분리

--- lib/screen/highlight_edit/title_field_bar.dart
+++ lib/screen/highlight_edit/title_field_bar.dart
@@ -5,42 +5,22 @@ import 'package:highlight_flutter/screen/highlight_edit/state/current_title_prov
 class TitleFieldBar extends ConsumerWidget {
   const TitleFieldBar({super.key});
 
-  static const titleFieldBorder = OutlineInputBorder(
-    borderRadius: BorderRadius.all(Radius.circular(4)),
-    borderSide: BorderSide(width: 0.1),
-  );
-
   static const maxTitleLength = 20;
 
   static const titleTextStyle = TextStyle(fontSize: 14);
 
-  static const titleLengthTextStyle = TextStyle(fontSize: 10);
-
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final currentTitle = ref.watch(currentTitleProvider);
     return Padding(
       padding: const EdgeInsets.all(8),
       child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           const Text('제목'),
           const SizedBox(width: 8),
           Expanded(
               child: TextField(
             style: titleTextStyle,
-            decoration: InputDecoration(
-              border: titleFieldBorder,
-              enabledBorder: titleFieldBorder,
-              focusedBorder: titleFieldBorder,
-              disabledBorder: titleFieldBorder,
-              focusedErrorBorder: titleFieldBorder,
-              counterText: '',
-              suffix: Text(
-                '${currentTitle.length}/$maxTitleLength',
-                style: titleLengthTextStyle,
-              ),
-              isDense: true,
-            ),
             cursorWidth: 1,
             maxLines: 1,
             maxLength: maxTitleLength,
