a008992e75a5e7b091392ad7d2485da3d4dd5a0e
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-08-12 18:48:29 +0900

    Feat: 사진 목록 상태 프로바이더 Notifier방식으로 변경

--- lib/screen/highlight_edit/state/current_picked_photos_provider.dart
+++ lib/screen/highlight_edit/state/current_picked_photos_provider.dart
@@ -1,7 +1,20 @@
 import 'package:image_picker/image_picker.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final currentPickedPhotosProvider = StateProvider.autoDispose<List<XFile>>(
-  (ref) => [],
+final currentPickedPhotosProvider =
+    NotifierProvider.autoDispose<CurrentPickedPhotos, List<XFile>>(
+  () => CurrentPickedPhotos(),
   name: '[Provider]CurrentPickedPhotos',
 );
+
+class CurrentPickedPhotos extends AutoDisposeNotifier<List<XFile>> {
+  @override
+  List<XFile> build() {
+    return [];
+  }
+
+  void addPhotos(List<XFile> newPhotos) {
+    state.addAll(newPhotos);
+    ref.notifyListeners();
+  }
+}
