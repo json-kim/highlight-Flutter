5b13854769145efd5b005e39ef72f18d1966bc43
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-08-14 02:06:58 +0900

    Feat: 뷰어화면 닫기 버튼 추가

--- lib/screen/common/photo_view_screen.dart
+++ lib/screen/common/photo_view_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:photo_view/photo_view_gallery.dart';
 
@@ -13,12 +14,20 @@ class PhotoViewScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: PhotoViewGallery.builder(
-          pageController: pageController,
-          itemCount: photos.length,
-          builder: (context, index) => PhotoViewGalleryPageOptions(
-              imageProvider: FileImage(File(photos[index].path)))),
+    return Stack(
+      children: [
+        PhotoViewGallery.builder(
+            pageController: pageController,
+            itemCount: photos.length,
+            builder: (context, index) => PhotoViewGalleryPageOptions(
+                imageProvider: FileImage(File(photos[index].path)))),
+        Positioned(
+            right: 8,
+            top: 24,
+            child: IconButton(
+                onPressed: context.pop,
+                icon: const Icon(Icons.close, size: 36))),
+      ],
     );
   }
 }
