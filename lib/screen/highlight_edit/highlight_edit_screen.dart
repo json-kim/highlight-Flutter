9b5268a11cc00d80a0689bded8668d37d772a23b
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-08-13 13:24:09 +0900

    Feat: 저장버튼 추가, 저장 이후 리스트 화면으로 돌아가는 동작 추가

--- lib/screen/highlight_edit/highlight_edit_screen.dart
+++ lib/screen/highlight_edit/highlight_edit_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:highlight_flutter/app/router/app_router.dart';
 import 'package:highlight_flutter/screen/highlight_edit/color_select_bar.dart';
 import 'package:highlight_flutter/screen/highlight_edit/content_field_bar.dart';
 import 'package:highlight_flutter/screen/highlight_edit/date_select_bar.dart';
@@ -8,10 +10,27 @@ import 'package:highlight_flutter/screen/highlight_edit/title_field_bar.dart';
 class HighlightEditScreen extends StatelessWidget {
   const HighlightEditScreen({super.key});
 
+  void saveHighlight() {
+    // TODO: 하이라이트 저장
+  }
+
+  void backToList(BuildContext context) {
+    context.go('/${Routes.list}');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
+      appBar: AppBar(
+        actions: [
+          TextButton(
+              onPressed: () {
+                saveHighlight();
+                backToList(context);
+              },
+              child: const Text('저장'))
+        ],
+      ),
       body: ListView(
         padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
         children: const [
