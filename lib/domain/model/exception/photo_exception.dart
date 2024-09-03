43e12744a3dfcef157d069e2d7dfa62caef1e338
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-09-04 02:32:16 +0900

    Feat: 예외 메시지 추가

--- lib/domain/model/exception/photo_exception.dart
+++ lib/domain/model/exception/photo_exception.dart
@@ -1,6 +1,10 @@
 class PhotoException implements Exception {
   PhotoException({this.message, this.trace});
 
+  static const notExist = '저장된 사진이 없습니다';
+  static const saveRequestFail = '사진 저장에 실패했습니다';
+  static const retrieveRequestFail = '사진을 가져오는데 실패했습니다';
+
   final String? message;
   final StackTrace? trace;
 
