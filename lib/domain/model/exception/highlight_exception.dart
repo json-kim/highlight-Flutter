7f8ba75139a5b1659f1b3c4f04d19809e7ef4442
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-09-04 02:32:16 +0900

    Feat: 예외 메시지 추가

--- lib/domain/model/exception/highlight_exception.dart
+++ lib/domain/model/exception/highlight_exception.dart
@@ -1,6 +1,10 @@
 class HighlightException implements Exception {
   HighlightException({this.message, this.trace});
 
+  static const notExist = '저장된 하이라이트가 없습니다';
+  static const saveRequestFail = '하이라이트 저장에 실패했습니다';
+  static const retrieveRequestFail = '하이라이트를 가져오는데 실패했습니다';
+
   final String? message;
   final StackTrace? trace;
 
