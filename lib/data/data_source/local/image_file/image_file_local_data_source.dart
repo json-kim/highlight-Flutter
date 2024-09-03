5b4508cee21bad45fdd977ef2f0a39a229aa255b
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-09-04 00:53:42 +0900

    Feat: 이미지 파일 로컬 데이터소스 클래스 추가

---
+++ lib/data/data_source/local/image_file/image_file_local_data_source.dart
@@ -0,0 +1,30 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+import 'package:path_provider/path_provider.dart';
+
+class ImageFileLocalDataSource {
+  Future<void> saveImage(XFile file) async {
+    final newPath = await getFileSavePath(file.path);
+
+    return _saveFile(file.path, newPath);
+  }
+
+  Future<void> _saveFile(String filePath, String newPath) async {
+    await File(filePath).copy(newPath);
+  }
+
+  Future<String> getFileSavePath(String filePath) async {
+    final directory = await getApplicationDocumentsDirectory();
+
+    return makeFilePath(directory.path, splitImageFileName(filePath));
+  }
+
+  String makeFilePath(String dir, String fileName) {
+    return '$dir/$fileName';
+  }
+
+  String splitImageFileName(String filePath) {
+    return filePath.split('/').last;
+  }
+}
