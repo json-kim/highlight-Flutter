43b4fc56c8e9b4785a09c877adb13f762da7c7ec
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-09-03 22:28:55 +0900

    Feat: HighlightsTable, PhotosTable Dao 클래스 추가

---
+++ lib/data/data_source/local/dao/highlights_dao.g.dart
@@ -0,0 +1,9 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'highlights_dao.dart';
+
+// ignore_for_file: type=lint
+mixin _$HighlightsDaoMixin on DatabaseAccessor<AppDatabase> {
+  $HighlightsTableTable get highlightsTable => attachedDatabase.highlightsTable;
+  $PhotosTableTable get photosTable => attachedDatabase.photosTable;
+}
