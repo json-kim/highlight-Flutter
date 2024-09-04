ca0f02623a757ebd62233c024b318680a5916a16
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-09-04 17:46:13 +0900

    Feat: 하이라이트 개수 프로바이더 추가

---
+++ lib/screen/profile/state/highlight_count_provider.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:highlight_flutter/domain/repository/repository_provider.dart';
+import 'package:highlight_flutter/domain/repository/result/api_result.dart';
+
+final highlightCountProvider =
+    AsyncNotifierProvider.autoDispose<HighlightCountProvider, int>(
+        () => HighlightCountProvider(),
+        name: '[Provider]HighlightCount');
+
+class HighlightCountProvider extends AutoDisposeAsyncNotifier<int> {
+  @override
+  FutureOr<int> build() {
+    return loadCount();
+  }
+
+  Future<int> loadCount() {
+    return _requestCount();
+  }
+
+  Future<int> _requestCount() async {
+    final result =
+        await ref.watch(highlightRepositoryProvider).countAllHighlight();
+
+    return switch (result) {
+      ApiSuccess() => result.data,
+      ApiFail() => 0,
+    };
+  }
+}
