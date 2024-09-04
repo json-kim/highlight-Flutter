5e2ebb0346514e6631c9e1b30ab0a2b11dc4fbb2
flutter_dart@kakao.com <flutter_dart@kakao.com>
2024-09-04 17:46:13 +0900

    Feat: 하이라이트 개수 프로바이더 추가

--- lib/screen/profile/profile_bar.dart
+++ lib/screen/profile/profile_bar.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:highlight_flutter/screen/profile/state/highlight_count_provider.dart';
 
 class ProfileBar extends StatelessWidget {
   const ProfileBar({super.key});
@@ -27,7 +29,7 @@ class ProfileBar extends StatelessWidget {
                 '내 하이라이트',
                 style: TextStyle(fontSize: 18),
               ),
-              Text('7'),
+              CountText(),
             ],
           ),
 
@@ -35,5 +37,21 @@ class ProfileBar extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CountText extends ConsumerWidget {
+  const CountText({super.key});
+
+  String countText(int count) {
+    return '$count 개';
+  }
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return ref.watch(highlightCountProvider).when(
+        data: (data) => Text(countText(data)),
+        error: (error, stackTrace) => Text(countText(0)),
+        loading: () => Text(countText(0)));
   }
 }
